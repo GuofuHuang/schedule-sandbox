d1a28e7783ac9ab210d0e16df820d981bf593fe6
Joe Phearse <yao522@my.utsa.edu>
2017-03-03 16:12:09 -0600

    schemas for collections

---
+++ both/models/customerBranch.model.ts
@@ -0,0 +1,21 @@
+export interface CustomerBranch {
+    _id?: string;
+    customerID: string;
+    CustomerName: string;
+    shipTo: string;
+    name: string;
+    address1: string;
+    address2: string;
+    address3: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    phone: string;
+    phoneExtension: string;
+    fax: string;
+    email: string;
+    createdUserID: string;
+    createdDate: Date;
+    updatedUserID: string;
+    updatedDate: Date;
+}
---
+++ both/models/vendor.model.ts
@@ -0,0 +1,20 @@
+export interface Vendor {
+    _id?: string;
+    vendor: string;
+    name: string;
+    address1: string;
+    address2: string;
+    address3: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    phone: string;
+    phoneExtension: string;
+    fax: string;
+    email: string;
+    createdUserID: string;
+    createdDate: Date;
+    updatedUserID: string;
+    updatedDate: Date;
+    vendorContracts: Array;
+}
