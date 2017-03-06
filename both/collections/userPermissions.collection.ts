16abda65cf7269202d3295dcd5b711d7f5894342
Joe Phearse <yao522@my.utsa.edu>
2017-03-06 09:24:09 -0600

    put export back to how it was

--- both/collections/userPermissions.collection.ts
+++ both/collections/userPermissions.collection.ts
@@ -1,7 +1,7 @@
 import { MongoObservable } from 'meteor-rxjs';
 import { UserPermission } from  '../models/userPermission.model';
 
-export const UserPermissions = new MongoObservable.Collection<UserPermission>('userPermissions');
+export const UserPermissions = new MongoObservable.Collection<any>('userPermissions');
 
 UserPermissions.deny({
   insert() { return true },
