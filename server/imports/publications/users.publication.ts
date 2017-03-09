e7922e135dd54d43de6834005395ec09164965b3
Joe Phearse <yao522@my.utsa.edu>
2017-03-09 09:01:18 -0600

    added publication for adminUsers

--- server/imports/publications/users.publication.ts
+++ server/imports/publications/users.publication.ts
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
+import { miniNumbers, totalNumbers} from './index';
 import { Users } from '../../../both/collections/users.collection';
 import { User } from '../../../both/models/user.model';
 
@@ -13,7 +14,13 @@ Meteor.publish('users', function(): Mongo.Cursor<User> {
 
   return Meteor.users.find({}, {
     fields: {
-      profile: 1
+      profile: 1,
+      username: 1,
+      emails: 1,
+      services: 1,
+      groups: 1,
+      manages: 1,
+      tenants: 1,
     }
   })
   // return Users.collection.find({}, {
@@ -26,7 +33,13 @@ Meteor.publish('users', function(): Mongo.Cursor<User> {
 Meteor.publish('currentUser', function() {
   return Users.collection.find(this.userId, {
     fields: {
-      profile: 1
+      profile: 1,
+      username: 1,
+      emails: 1,
+      services: 1,
+      groups: 1,
+      manages: 1,
+      tenants: 1,
     }
   })
 })
@@ -36,3 +49,40 @@ Meteor.publish('groups', function() {
   return UserGroups.collection.find({});
 
 })
+
+Meteor.publish('adminUsers', function(selector: any, options: any, keywords: string) {
+  let fields = options.fields;
+
+  let select;
+  if (!keywords || keywords == '') {
+    select = selector;
+  } else {
+    select = generateRegex(fields, keywords);
+    select.tenantId = selector.tenantId;
+  }
+
+  miniNumbers['users'] = Users.collection.find(select).count();
+  totalNumbers['users'] = Users.collection.find({tenantId: selector.tenantId}).count();
+
+  console.log(Users.find(select).cursor.count());
+  this.onStop(() => {
+    console.log('it is stopped');
+  });
+
+  options.fields.tenantId = 1;
+
+  return Users.collection.find(select, options);
+});
+
+function generateRegex(fields: Object, keywords) {
+  let obj = {
+    $or: []
+  };
+  Object.keys(fields).forEach((key, index) => {
+    obj.$or.push({
+      [key]: {$regex: new RegExp(keywords, 'i')}
+    })
+
+  });
+  return obj;
+}
