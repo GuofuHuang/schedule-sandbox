9a1a7a6825c76682ae304dd1ae7badf9bd101ee8
Joe Phearse <yao522@my.utsa.edu>
2017-03-21 16:47:28 -0500

    files for admin-systemLookup

---
+++ client/imports/pages/admin-systemLookup/admin-systemLookup.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SystemLookups } from '../../../../both/collections/index';
+import { Users } from '../../../../both/collections/users.collection';
+
+import template from './admin-systemLookup.component.html';
+import style from './admin-systemLookup.component.scss';
+
+@Component({
+  selector: 'admin-systemLookup',
+  template,
+  styles: [ style ]
+})
+
+export class systemLookupComponent implements OnInit{
+
+  @Input() data: any;
+  systemLookupCollections: any[];
+  systemLookupLookupName: string;
+
+  constructor() {}
+
+  ngOnInit() {
+
+    this.systemLookupCollections = [SystemLookups];
+    this.systemLookupLookupName = 'systemLookups';
+
+
+
+  }
+}
