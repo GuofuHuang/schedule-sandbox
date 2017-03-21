e23ae3ca09d8be3d73b8526d4200decddc2beb2d
Joe Phearse <yao522@my.utsa.edu>
2017-03-21 10:47:45 -0500

    changed file path

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -22,8 +22,8 @@ import { GLOBALSEARCH_DECLARATIONS } from '../components/global-search';
 import { USERDROPDOWN_CLERATIONS } from '../components/user-dropdown';
 import { HEADERBAR_DECLARATIONS } from '../components/headerbar';
 import { CUSTOMERINQUIRY_DECLARATIONS } from '../components/customer-inquiry';
-import { ADMINUSERS_DECLARATIONS } from '../components/admin-users';
-import { ADMINEACHUSERS_DECLARATIONS } from '../components/admin-eachUser';
+import { ADMINUSERS_DECLARATIONS } from '../pages/admin-users';
+import { ADMINEACHUSERS_DECLARATIONS } from '../pages/admin-eachUser';
 import { CUSTOMERMEETINGS_DECLARATIONS } from '../pages/customer-meetings';
 import { MULTISYSTEMLOOKUP_DECLARATIONS } from '../components/multi-system-lookup';
 
