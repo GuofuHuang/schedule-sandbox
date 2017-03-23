521c24f4c1fbe3e78e62f82b0915d2234cae1bbb
Joe Phearse <yao522@my.utsa.edu>
2017-03-23 10:52:50 -0500

    commented out customers lookup

--- client/imports/pages/admin-systemLookup/admin-systemLookup.component.ts
+++ client/imports/pages/admin-systemLookup/admin-systemLookup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SystemLookups } from '../../../../both/collections/index';
 import { Users } from '../../../../both/collections/users.collection';
 
+// import { Customers } from '../../../../both/collections/customers.collection';
+
 import template from './admin-systemLookup.component.html';
 import style from './admin-systemLookup.component.scss';
 
@@ -17,9 +19,15 @@ export class systemLookupComponent implements OnInit{
   systemLookupCollections: any[];
   systemLookupLookupName: string;
 
+  // customerCollections: any[];
+  // customerLookupName: string;
+
+
   constructor() {}
 
   ngOnInit() {
+    // this.customerCollections = [Customers];
+    // this.customerLookupName = 'customers';
 
     this.systemLookupCollections = [SystemLookups];
     this.systemLookupLookupName = 'systemLookup';
