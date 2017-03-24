bc32fb42c6db8caa68575f7f5e27b79073e7806a
Joe Phearse <yao522@my.utsa.edu>
2017-03-24 10:42:36 -0500

    adding permission layout

--- client/imports/pages/admin-permissions/admin-permissions.page.ts
+++ client/imports/pages/admin-permissions/admin-permissions.page.ts
@@ -16,6 +16,7 @@ export class adminPermissionsPage implements OnInit{
   @Input() data: any;
   // userCollections: any[];
   // userLookupName: string;
+  dataObj: {}
 
   constructor(public dialog: MdDialog) {}
 
@@ -26,4 +27,36 @@ export class adminPermissionsPage implements OnInit{
 
 
   }
+  addPemission() {
+  console.log(generateMongoID())
+
+  let permissionNameInput = (<HTMLInputElement>document.getElementById("permissionNameInput")).value;
+  // console.log(firstNameInput)
+  let permissionDescriptionInput = (<HTMLInputElement>document.getElementById("permissionDescriptionInput")).value;
+  // console.log(lastNameInput)
+  let permissionUrlInput = (<HTMLInputElement>document.getElementById("permissionUrlInput")).value;
+  // console.log(lastNameInput)
+
+  this.dataObj = {
+    id: generateMongoID(),
+    name: permissionNameInput,
+    description: permissionDescriptionInput,
+    url: permissionUrlInput,
+    createdId: Meteor.userId(),
+    createdDate: new Date()
+  }
+  console.log(this.dataObj)
+  MeteorObservable.call('addPermission', this.dataObj).subscribe(permissionInfo => {})
+  }
+}
+
+function generateMongoID () {
+  var mongoID = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < 17; i++ ) {
+    mongoID += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return mongoID;
 }
