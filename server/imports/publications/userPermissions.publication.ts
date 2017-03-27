ecec89011f6581094ecb54458a0983d511b3946c
Joe Phearse <yao522@my.utsa.edu>
2017-03-27 10:25:15 -0500

    imported count

--- server/imports/publications/userPermissions.publication.ts
+++ server/imports/publications/userPermissions.publication.ts
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { Counts } from 'meteor/tmeasday:publish-counts';
 
 import { UserPermissions } from '../../../both/collections/userPermissions.collection';
 import { UserPermission } from '../../../both/models/userPermission.model';
 
 Meteor.publish('permissions', function(): Mongo.Cursor<UserPermission> {
+  Counts.publish(this, 'permissions', UserPermissions.find({}).cursor, {noReady: false});
 
   return UserPermissions.collection.find({}, {limit: 10});
 });
