fa429e9cff1ff86c234216659ec40ba4ae2005e7
Guofu Huang <wzcnhgf@gmail.com>
2017-03-31 09:17:16 -0500

    delete old system-lookup component, rename multi-system-lookup to system-lookup

--- client/imports/components/multi-system-lookup/index.ts
+++
@@ -1,5 +0,0 @@
-import { MultiSystemLookup } from './multi-system-lookup.component';
-
-export const MULTISYSTEMLOOKUP_DECLARATIONS = [
-  MultiSystemLookup
-]
--- client/imports/components/system-lookup/system-lookup.component.ts
+++ client/imports/components/system-lookup/system-lookup.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit, OnDestroy, Input, Output, EventEmitter} from '@angular/core';
-import {MeteorObservable} from 'meteor-rxjs';
-import { Counts } from 'meteor/tmeasday:publish-counts';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-import { Observable } from 'rxjs/Observable';
+import { MeteorObservable } from "meteor-rxjs";
+import { Counts } from 'meteor/tmeasday:publish-counts';
 
-import { SystemLookups } from '../../../../both/collections';
 import template from './system-lookup.component.html';
+import { SystemLookups } from '../../../../both/collections';
+import { SystemLookup } from '../../../../both/models/systemLookup.model';
+import Dependency = Tracker.Dependency;
 
 @Component({
   selector: 'system-lookup',
@@ -14,252 +15,278 @@ import template from './system-lookup.component.html';
 
 export class SystemLookupComponent implements OnInit, OnDestroy {
   @Input() Collections: any[];
-  @Input() Collection: any;
+  @Input() collection: any;
   @Input() lookupName: string;
   @Output() onSelected = new EventEmitter<string>();
+  @Output() onReturn = new EventEmitter<{}>();
 
   rows: any[] = []; // row data to be displayed in the data table
   columns: any[] = []; // headers in the data table
-  systemLookup: any; // data retrieved from system lookup collection, contains all data
-  displayedFields: any; // fields data will be displayed in the data table
-  returnedFields: any; // fields data will be returned as string result to the customer
-  collectionName: string; // collection name for the lookup
-  allFields: Object[]; // data retrieved from collection including hidden field in the data table
   selector: any = {}; // selector for the mognodb collection search
-  keywordsDep: Tracker.Dependency = new Tracker.Dependency(); // keywords dependency to invoke a search function
-  keywords: string; // keywords to search the database
+  keywords: string = ''; // keywords to search the database
 
   start: number = 0; // start index for the rows displayed in the data table
-  count: number = 0; // count for the data table
+  count: number = 10; // count for the data table
   offset: number = 0; // offset for the data table
   limit: number = 0; // limit for the data table
-  offsetArr: {} = {0:0}; // offset array for the data added to the data table
+  skip: number = 0;
   messages: any; // messages for data table
-  handle: any; // handle the subscription
+  handles: Subscription[]; // all subscription handles
+  systemLookup: any = {};
+  dataTableOptions: any = {};
+  returnData: string[];
+  selected: any[] = [];
+  lookupDep: Dependency = new Dependency(); // keywords dependency to invoke a search function
+  keywordsDep: Dependency = new Dependency(); // keywords dependency to invoke a search function
 
   constructor() {}
 
   ngOnInit() {
-
-    let search;
-
-    Session.set('keywords', search);
-
-
-
-    this.selector = {tenantId: Session.get('tenantId')};
-
     this.messages = {
       emptyMessage: 'no data available in table',
       totalMessage: 'total'
     };
+    this.handles = [];
 
-    this.displayedFields = {
-      fields: {},
-      limit: Number,
-      sort: {}
-    };
+    this.collection = this.Collections[0];
 
-    this.systemLookup = {
-      dataTableColumns: [],
-      findOptions: {
-        sort: {}
-      }
-    }
+    let handle = MeteorObservable.autorun().subscribe(() => {
 
-    if (this.Collections.length == 1) {
+      this.handles.push(MeteorObservable.subscribe('systemLookups', this.lookupName, Session.get('tenantId')).subscribe());
 
-      this.collectionName = this.Collections[0]._collection._name;
-      MeteorObservable.subscribe('systemLookups', this.lookupName).subscribe(() => {
-        MeteorObservable.autorun().subscribe(() => {
+      // console.log(this.systemLookup);
+      this.systemLookup = SystemLookups.collection.findOne({
+        name: this.lookupName,
+        tenantId: Session.get('tenantId')
+      });
+
+      let handle = MeteorObservable.autorun().subscribe(() => {
+
+        // put dependency here
+
+        this.keywordsDep.depend();
+        this.lookupDep.depend();
+
+        // this.dataTableOptions = this.systemLookup.dataTableOptions;
+
+        if (this.systemLookup) {
+          this.columns = this.getColumnsM(this.systemLookup);
+          this.dataTableOptions = this.systemLookup.dataTable.table;
+
+          // {
+          //   $$index: 0,
+          //   _id: "44RccJQuPpxuAjw6o",
+          //   tenants: [
+          //     "4sdRt09goRP98e456"
+          //   ],
+          //   username: "NO EMAIL"
           //
-          this.systemLookup = SystemLookups.find({name: this.lookupName}).cursor
-            .fetch()[0];
-          this.limit = this.systemLookup.findOptions.limit;
-          this.getColumns();
-          this.getTableData(this.selector, this.systemLookup);
-        });
-      })
-    }
-  }
+          // }
+          this.selected = [];
+          if (this.Collections.length > 1) {
 
-  getTableData(selector, systemLookup) {
-    this.makeSubcription();
+            this.systemLookup.multi.pipeline = this.processPipeline(this.systemLookup.multi.pipeline);
+            this.getRowsM(this.systemLookup, this.columns, this.keywords);
+          } else {
+            this.limit = this.systemLookup.single.findOptions.limit;
+            // set rows inside this function
+            this.getRows(this.systemLookup, this.columns, this.keywords);
+            this.selected = [];
 
-    MeteorObservable.autorun().subscribe(() => {
-
-      this.keywordsDep.depend();
-
-      let fields = systemLookup.findOptions.fields;
-
-      if (!this.keywords || this.keywords == '') {
-        this.selector = selector;
-      } else {
-        this.selector = this.generateRegex(fields, this.keywords);
-      }
-      this.getRows();
-    })
-  }
-
-  getColumns() {
-    let arr = [];
-
-    this.returnedFields = [];
-    // select displayed columns to data table
-    this.systemLookup.dataTableColumns.forEach((column, index) => {
-      if (!column.hidden) {
-        let obj = {
-          prop: column.prop,
-          name: column.name
+          }
         }
+      })
+      this.handles.push(handle);
+    });
+    this.handles.push(handle);
+
+  }
+
+  getColumnsM(systemLookup:any) {
+    let arr = [];
+    // select displayed columns to data table
+
+    systemLookup.dataTable.columns.forEach((column, index) => {
+      let obj = {};
+      if (!column.hidden) {
+        Object.keys(column).forEach(key => {
+          obj[key] = column[key];
+        });
         arr.push(obj);
-        this.displayedFields.fields[column.prop] = 1;
       }
-      if (column.returned) {
-        this.returnedFields[index] = column.prop;
-      }
-
     });
 
-    this.columns = arr;
+    return arr;
   }
 
-  getRows() {
-    // this code is required get the data reactively
+  // get rows for multiple system lookup
+  getRowsM(systemLookup, columns, keywords) {
+    let arr = [];
+    let handle = MeteorObservable.call('getAggregations', Session.get('tenantId'), this.collection._collection._name,
+      systemLookup.multi.pipeline, columns, keywords)
+        .subscribe((res:any[]) => {
+          res.forEach((row, index) => {
+            let obj = {};
+            columns.forEach((column) => {
+              obj[column.prop] = row[column.prop];
+            });
+            arr.push(obj);
+          });
+          this.limit = 10;
+          this.count = arr.length;
+          this.rows = arr;
+        });
 
-    this.displayedFields.limit = this.limit;
-    if (this.systemLookup.hasOwnProperty('findOptions')) {
-      if (this.systemLookup.findOptions.hasOwnProperty('sort')) {
 
-        this.displayedFields.sort = this.systemLookup.findOptions.sort;
-      }
-    }
+    this.handles.push(handle);
+    return arr;
+  }
 
-    this.rows = [];
+  // get rows for single system lookup
+  getRows(systemLookup, columns, keywords) {
+    let selector = this.getSelector(systemLookup);
+    let options = systemLookup.single.findOptions;
 
-    if (this.collectionName == 'users') {
-      this.selector.tenants = {$in: [Session.get('tenantId')]};
-      delete this.selector['tenantId'];
-    }
+    let handle = MeteorObservable.subscribe(this.lookupName, selector, options, keywords).subscribe();
 
-    this.displayedFields.skip = 0;
+      MeteorObservable.autorun().subscribe(() => {
+        let fields = options.fields;
+        let select;
 
-    this.Collections[0].find(this.selector, this.displayedFields).cursor
-      .map((doc, index) => {
-        this.rows[this.start+index] = doc;
+        if (!keywords || keywords == '') {
+          select = selector;
+        } else {
+          select = generateRegex(fields, keywords);
+        }
+
+        this.rows = [];
+        options.skip = 0;
+        this.collection.collection.find(select, options).forEach((item, index) => {
+          this.rows[this.skip + index]= item;
+        });
+
+        this.count = Counts.get(this.lookupName);
       })
 
-    // empty the fields option to get all fields
-    this.displayedFields.fields = {};
-    this.allFields = this.Collections[0].find(this.selector, this.displayedFields).cursor
-      .fetch();
-    this.count = Counts.get(this.lookupName);
+
+    this.handles.push(handle);
+
   }
 
-  makeSubcription() {
-    if (this.collectionName == 'users') {
-      this.selector.tenants = {$in: [Session.get('tenantId')]};
-      delete this.selector['tenantId'];
+  getSelector(systemLookup) {
+    let fields = systemLookup.single.findOptions.fields;
+    let selector = {};
+    if ('tenantId' in fields) {
+      selector = { tenantId: Session.get('tenantId')};
+
+    } else if ('tenants' in fields) {
+      selector = { tenants: {$in: [Session.get('tenantId')]}};
     }
-
-    this.handle = Meteor.subscribe(this.lookupName, this.selector, this.systemLookup.findOptions, this.keywords);
+    return selector;
   }
 
-  generateRegex(fields: Object, keywords: string) {
-    console.log(fields);
-    let obj = {
-      $or: []
-    };
-    Object.keys(fields).forEach((key) => {
-      obj.$or.push({
-        [key]: {$regex: new RegExp(keywords, 'i')}
-      })
-    });
-    console.log(obj);
-
+  processPipeline(obj:any) {
+    obj = JSON.stringify(obj);
+    obj = obj.replace(/_\$/g, '$');
+    obj = JSON.parse(obj);
     return obj;
   }
 
-  onSelect(event) {
-    this.systemLookup = '';
-    let index = event.selected[0].$$index - this.offset*this.limit;
-    let selected = this.allFields[index];
-    let result;
+  search(keywords) {
+    this.keywords = keywords;
+    this.lookupDep.changed();
 
-    // loop through the returnFields
-    for (let i = 0; i < this.returnedFields.length; i++){
-      if (i == 0) {
-        result = selected[this.returnedFields[i]];
-      } else {
-        result = result + ' - ' + selected[this.returnedFields[i]];
+  }
+
+  onSelect(event) {
+    if (this.Collections.length == 1) {
+      if ('returnData' in this.systemLookup.single && this.systemLookup.single.returnData.length > 0) {
+        let selected = event.selected[0];
+        let index = event.selected[0].$$index - this.offset*this.limit;
+        let result = '';
+
+        this.returnData = this.systemLookup.single.returnData;
+
+        this.returnData.forEach(field => {
+          if (field in selected) {
+            result += selected[field];
+          } else {
+            result += field;
+          }
+        })
+
+        // loop through the returnFields
+        this.onSelected.emit(result);
+
       }
+
     }
-    console.log(result);
-    this.onSelected.emit(result);
+  }
+
+  add() {
+    this.selected = [this.rows[1], this.rows[3]];
+  }
+
+  remove() {
+    this.selected = [];
   }
 
   onSort(event) {
-
-    this.handle.stop();
-    this.displayedFields.skip = 0;
-    this.start = 0;
+    let sortProp = event.sorts[0].prop;
     this.offset = 0;
-    this.systemLookup.findOptions.skip = 0;
-    this.offsetArr = {0:0};
-    let temp = event.sorts[0].prop;
-    this.systemLookup.findOptions.sort = {};
-    if (event.sorts[0].dir == 'asc') {
-      this.systemLookup.findOptions.sort[temp] = 1;
+    this.skip = 0;
+
+    if (this.Collections.length > 1) {
+      let sort = {$sort: {}};
+
+      if (event.sorts[0].dir == 'asc') {
+        sort.$sort[sortProp] = 1;
+      } else {
+        sort.$sort[sortProp] = -1;
+      }
+
+      this.systemLookup.multi.pipeline.push(sort);
     } else {
-      this.systemLookup.findOptions.sort[temp] = -1;
+      this.systemLookup.single.findOptions.skip = 0;
+      this.systemLookup.single.findOptions.sort = {};
+      if (event.sorts[0].dir == 'asc') {
+        this.systemLookup.single.findOptions.sort[sortProp] = 1;
+      } else {
+        this.systemLookup.single.findOptions.sort[sortProp] = -1;
+      }
     }
 
-    this.getColumns();
 
-    this.getTableData(this.selector, this.systemLookup);
+    this.lookupDep.changed();
   }
 
   onPage(event) {
-
-    this.start = event.offset * this.limit;
-
-    let i =0;
-    Object.keys(this.offsetArr).forEach((value, index) => {
-
-      if (value < event.offset) {
-        i++;
-      }
-    })
-
-    this.displayedFields.skip = i * this.limit;
-    this.offsetArr[event.offset] = event.offset;
-
-    event.limit = undefined;
-    this.systemLookup.findOptions.skip = event.offset * this.systemLookup.findOptions.limit;
     this.offset = event.offset;
-
-    this.handle.stop();
-    this.makeSubcription();
-    this.getRows();
-  }
-
-  search(keywords) {
-    if (keywords == '') {
-      keywords = null;
-    }
-    this.displayedFields.skip = 0;
-    this.start = 0;
-    this.offset = 0;
-    this.systemLookup.findOptions.skip = 0;
-    this.keywords = keywords;
-    this.selector = {tenantId: Session.get('tenantId')};
-    this.handle.stop();
-
-    this.makeSubcription();
+    this.skip = event.offset * event.limit;
+    this.systemLookup.single.findOptions.skip = this.skip;
     this.keywordsDep.changed();
   }
 
-  ngOnDestroy() {
-    this.handle.stop();
+  save() {
+
+    this.onReturn.emit({});
   }
+
+  ngOnDestroy() {
+    this.handles.forEach(handle => {
+      handle.unsubscribe();
+    })
+  }
+}
+
+function generateRegex(fields: Object, keywords) {
+  let obj = {
+    $or: []
+  };
+  Object.keys(fields).forEach((key, index) => {
+    obj.$or.push({
+      [key]: {$regex: new RegExp(keywords, 'i')}
+    })
+  });
+  return obj;
 }
