87bb9886f478560cf62852ca8653cc25410963fc
Joe Phearse <yao522@my.utsa.edu>
2017-04-04 16:27:56 -0500

    logic for gathering edits and updating document

--- client/imports/pages/admin-eachSystemLookup/admin-eachSystemLookup.page.ts
+++ client/imports/pages/admin-eachSystemLookup/admin-eachSystemLookup.page.ts
@@ -21,10 +21,25 @@ export class eachSystemLookupPage implements OnInit{
   name: string;
   collection: string;
   label: string;
+  lookup: string;
+  query: string;
+  dataTable: string;
+
+  nameInput: string;
+  collectionInput: string;
+  labelInput: string;
   searchable: boolean;
-  default: boolean;
+  queryInput: string;
+  dataTableInput: string;
+
+  // lookups = [
+  //   {value: 'single', viewValue: 'Single'},
+  //   {value: 'multi', viewValue: 'Multi'},
+  //   {value: 'fieldUpdate', viewValue: 'Field Update'}
+  // ];
 
   dataObj: {}
+  inputObj: {}
 
   constructor(private route: ActivatedRoute) {}
 
@@ -37,14 +52,46 @@ export class eachSystemLookupPage implements OnInit{
     MeteorObservable.call('returnLookup', this.lookupID).subscribe(lookupInfo => {
       console.log(lookupInfo);
       if (lookupInfo !== undefined) {
-        this.name = lookupInfo["name"]
-        this.collection = lookupInfo["collection"]
-        this.label = lookupInfo["label"]
+        this.nameInput = lookupInfo["name"]
+        this.collectionInput = lookupInfo["collection"]
+        this.labelInput = lookupInfo["label"]
         this.searchable = lookupInfo["searchable"];
-        this.default = lookupInfo["default"];
+        this.lookup = lookupInfo["lookupType"];
+        this.query = lookupInfo["query"];
+        this.dataTable = lookupInfo["dataTable"];
       }
+      console.log(JSON.stringify(this.query))
+      console.log(JSON.stringify(this.dataTable))
+
+      this.queryInput = JSON.stringify(this.query, undefined, 4)
+      this.dataTableInput = JSON.stringify(this.dataTable, undefined, 4)
     })
 
+
+
+  }
+
+  onBlurMethod(){
+    let query = JSON.parse(this.queryInput)
+    let dataTable = JSON.parse(this.dataTableInput)
+
+    console.log(this.nameInput)
+    this.inputObj = {
+      name: this.nameInput,
+      collection: this.collectionInput,
+      label: this.labelInput,
+      searchable: this.searchable,
+      query: query,
+      dataTable: dataTable
+    }
+    console.log(this.inputObj)
+    MeteorObservable.call('updateDocument', 'systemLookups', this.lookupID, this.inputObj).subscribe(updateLookup => {})
+  }
+
+  checkbox(event) {
+    console.log("check")
+    console.log(this.searchable)
+    // MeteorObservable.call('deleteSystemLookups', this.lookupID).subscribe(deleteLookup => {})
   }
 
   deleteLookup(event) {
