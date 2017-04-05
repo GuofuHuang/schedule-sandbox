ed25f1b34de23b810f39ffb6667778d0bd6a3ab6
Joe Phearse <yao522@my.utsa.edu>
2017-04-05 11:04:31 -0500

    delete button hides if not developer or lookup is default

--- client/imports/pages/admin-eachSystemLookup/admin-eachSystemLookup.page.ts
+++ client/imports/pages/admin-eachSystemLookup/admin-eachSystemLookup.page.ts
@@ -18,12 +18,12 @@ export class eachSystemLookupPage implements OnInit{
 
   @Input() data: any;
   lookupID: string;
-  name: string;
-  collection: string;
-  label: string;
+
   lookup: string;
   query: string;
   dataTable: string;
+  default: boolean = false;
+  developer: boolean = false;
 
   nameInput: string;
   collectionInput: string;
@@ -31,12 +31,6 @@ export class eachSystemLookupPage implements OnInit{
   searchable: boolean;
   queryInput: string;
   dataTableInput: string;
-
-  // lookups = [
-  //   {value: 'single', viewValue: 'Single'},
-  //   {value: 'multi', viewValue: 'Multi'},
-  //   {value: 'fieldUpdate', viewValue: 'Field Update'}
-  // ];
 
   dataObj: {}
   inputObj: {}
@@ -59,9 +53,12 @@ export class eachSystemLookupPage implements OnInit{
         this.lookup = lookupInfo["lookupType"];
         this.query = lookupInfo["query"];
         this.dataTable = lookupInfo["dataTable"];
+        this.default = lookupInfo["default"];
       }
-      console.log(JSON.stringify(this.query))
-      console.log(JSON.stringify(this.dataTable))
+      MeteorObservable.call('userHasPermission', "developerPermission").subscribe(permission => {
+        let developer = (permission === "enabled") ? true : false;
+        this.developer = developer
+      })
 
       this.queryInput = JSON.stringify(this.query, undefined, 4)
       this.dataTableInput = JSON.stringify(this.dataTable, undefined, 4)
@@ -75,7 +72,6 @@ export class eachSystemLookupPage implements OnInit{
     let query = JSON.parse(this.queryInput)
     let dataTable = JSON.parse(this.dataTableInput)
 
-    console.log(this.nameInput)
     this.inputObj = {
       name: this.nameInput,
       collection: this.collectionInput,
@@ -84,22 +80,16 @@ export class eachSystemLookupPage implements OnInit{
       query: query,
       dataTable: dataTable
     }
-    console.log(this.inputObj)
+    // console.log(this.inputObj)
     MeteorObservable.call('updateDocument', 'systemLookups', this.lookupID, this.inputObj).subscribe(updateLookup => {})
-  }
-
-  checkbox(event) {
-    console.log("check")
-    console.log(this.searchable)
-    // MeteorObservable.call('deleteSystemLookups', this.lookupID).subscribe(deleteLookup => {})
   }
 
   deleteLookup(event) {
     console.log("deleted")
-    console.log(Meteor.userId())
-    MeteorObservable.call('returnUser', Meteor.userId()).subscribe(userInfo => {
-      console.log(userInfo)
-    })
-    // MeteorObservable.call('deleteSystemLookups', this.lookupID).subscribe(deleteLookup => {})
+    // console.log(Meteor.userId())
+    // MeteorObservable.call('returnUser', Meteor.userId()).subscribe(userInfo => {
+    //   console.log(userInfo)
+    // })
+    MeteorObservable.call('deleteSystemLookups', this.lookupID).subscribe(deleteLookup => {})
   }
 }
