cd51444efca03db006761d39c7f4361d8dc828c2
Joe Phearse <yao522@my.utsa.edu>
2017-04-18 15:11:25 -0500

    modified methods for groupPermissions

--- server/imports/methods/methods.ts
+++ server/imports/methods/methods.ts
@@ -234,27 +234,26 @@ Meteor.methods({
   },
 
   adminAddGroupsPermissions(permissionName) {
-    let update = {
-      $set: {
-        [permissionName]: 'disabled'
-      }
-    };
-
     return UserGroups.update({},
-      update,
+      {
+        $push: {
+           groupPermissions: {
+             name: permissionName,
+             value: "disasbled"
+           }
+         }
+        },
 	    { multi: true }
     )
   },
 
   adminRemoveGroupsPermissions(permissionName) {
-    let update = {
-      $unset: {
-        [permissionName]: ''
-      }
-    };
-
     return UserGroups.update({},
-      update,
+      {
+        $pull: {
+           groupPermissions: {name: permissionName}
+         }
+        },
 	    { multi: true }
     )
   },
