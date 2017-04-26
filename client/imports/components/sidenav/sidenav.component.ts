b4dbecab16a8aad33395d4754c5ddfb6259b0d4c
Guofu Huang <wzcnhgf@gmail.com>
2017-04-26 13:55:30 -0500

    clean up code

--- client/imports/components/sidenav/sidenav.component.ts
+++ client/imports/components/sidenav/sidenav.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MeteorObservable } from 'meteor-rxjs';
 import { Router } from '@angular/router';
 import { Session } from 'meteor/session';
+import { Subscription } from 'rxjs/Subscription';
 
 import { SystemOptions } from '../../../../both/collections/systemOptions.collection';
 import { UserGroups } from '../../../../both/collections/userGroups.collection';
@@ -18,42 +19,41 @@ export class SidenavComponent implements OnInit {
   menus: any = [];
   subMenus: any;
   selectedMenu: any = {};
+  subscriptions: Subscription[];
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     // subscribe to collections to get updated automatically.
 
-    // this.getSelectedMenuName();
-    // Session.set('selectedMenu', this.getSelectedMenuName());
     let selectedMenu = this.getSelectedMenuName();
     if (selectedMenu !== '') {
       this.selectedMenu.name = selectedMenu;
     }
 
 
-    MeteorObservable.subscribe('systemOptions', Session.get('tenantId')).subscribe();
-    // MeteorObservable.subscribe('groups', Session.get('tenantId')).subscribe();
-    MeteorObservable.autorun().subscribe(() => {
+    this.subscriptions[0] = MeteorObservable.subscribe('systemOptions', Session.get('tenantId')).subscribe(() => {
+      this.subscriptions[1] = MeteorObservable.autorun().subscribe(() => {
+        SystemOptions.collection.find({}, {
+          fields: {
+            'value.name': 1,
+            'value.label': 1,
+            'value.permissionName': 1
+          }}).fetch();
 
-      SystemOptions.collection.find({}, {
-        fields: {
-          'value.name': 1,
-          'value.label': 1,
-          'value.permissionName': 1
-        }}).fetch();
-
-      MeteorObservable.call('getMenus', 'sidenav', Session.get('tenantId')).subscribe((res:any = []) => {
-        this.menus = res;
-        if (selectedMenu) {
-          res.some(menu => {
-            if (menu.name == this.selectedMenu.name) {
-              this.selectedMenu = menu;
-            }
-          })
-        }
-      })
+        MeteorObservable.call('getMenus', 'sidenav', Session.get('tenantId')).subscribe((res:any = []) => {
+          this.menus = res;
+          if (selectedMenu) {
+            res.some(menu => {
+              if (menu.name == this.selectedMenu.name) {
+                this.selectedMenu = menu;
+              }
+            })
+          }
+        })
+      });
     });
+
 
     MeteorObservable.autorun().subscribe(() => {
       SystemOptions.collection.find({}, {
