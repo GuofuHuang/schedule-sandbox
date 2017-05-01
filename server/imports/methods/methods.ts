51d78992e45cd0a9c14a79a1e6ad79072028c936
Joe Phearse <yao522@my.utsa.edu>
2017-05-01 14:51:30 -0500

    updated method for correct time stamps

--- server/imports/methods/methods.ts
+++ server/imports/methods/methods.ts
@@ -158,10 +158,10 @@ Meteor.methods({
           "description": permissionInfo.description,
           "url": permissionInfo.url,
           "tenantId": permissionInfo.tenantId,
-          "createdUserID": Meteor.userId(),
-          "createdDate": new Date(),
-          "updatedUserID": "",
-          "updatedDate": ""
+          "createdUserId": Meteor.userId(),
+          "createdAt": new Date(),
+          "updatedUserId": "",
+          "updatedAt": ""
       })
   },
 
