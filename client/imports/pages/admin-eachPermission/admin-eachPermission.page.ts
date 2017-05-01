c5763fa7179279148acf9fa851b3fa0c6e687666
Joe Phearse <yao522@my.utsa.edu>
2017-05-01 14:52:32 -0500

    updated to use correct time stamps

--- client/imports/pages/admin-eachPermission/admin-eachPermission.page.ts
+++ client/imports/pages/admin-eachPermission/admin-eachPermission.page.ts
@@ -102,8 +102,8 @@ export class adminEachPermissionPage implements OnInit{
         name: nameInput,
         description: descriptionInput,
         url: urlInput,
-        updatedUserID: Meteor.userId(),
-        updatedDate: new Date()
+        updatedUserId: Meteor.userId(),
+        updatedAt: new Date()
       }
       this._service.success(
         "Permission Updated",
