86083967bbb0e1400636d4acdf85c9b88d972490
Joe Phearse <yao522@my.utsa.edu>
2017-05-08 11:19:54 -0500

    fixed users route

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -27,7 +27,7 @@ export const routes: Route[] = [
       { path: 'create-quote', component: CreateQuotePage },
       { path: 'customerInquiry', component: CustomerInquiryComponent },
       { path: 'admin/users', component: adminUsersPage },
-      { path: 'adminUsers/:userID', component: adminEachUserComponent },
+      { path: 'admin/users/:userID', component: adminEachUserComponent },
       { path: 'admin/groups', component: AdminGroupsComponent },
       { path: 'admin/groups/:groupID', component: adminEachGroupPage },
       { path: 'admin/permissions', component: adminPermissionsPage },
