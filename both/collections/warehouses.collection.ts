b7bd821d48516461b3d2db93044af2853327f359
Joe Phearse <yao522@my.utsa.edu>
2017-05-18 16:27:29 -0500

    fixed export

--- both/collections/warehouses.collection.ts
+++ both/collections/warehouses.collection.ts
@@ -1,4 +1,4 @@
 import { MongoObservable } from 'meteor-rxjs';
 import { Warehouse } from  '../models/warehouse.model';
 
-export const Warehouses = new MongoObservable.Collection<any>('Warehouses');
+export const Warehouses = new MongoObservable.Collection<any>('warehouses');
