91b812490587ffc769027e659448db7ea4fb3ce2
Joe Phearse <yao522@my.utsa.edu>
2017-05-18 16:30:28 -0500

    added system-query, fixed routes

--- client/imports/pages/inventory-warehouse/inventory-warehouse.page.ts
+++ client/imports/pages/inventory-warehouse/inventory-warehouse.page.ts
@@ -5,8 +5,6 @@ import { NotificationsService } from 'angular2-notifications';
 import {MdDialog} from '@angular/material';
 import { DialogSelect } from '../../components/system-query/system-query.component';
 
-import { Users } from '../../../../both/collections/users.collection';
-
 import {filterDialogComponent} from '../../components/filterDialog/filterDialog.component';
 
 import template from './inventory-warehouse.page.html';
@@ -19,11 +17,6 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class InventoryWarehousePage implements OnInit{
 
-  userCollections: any[];
-  userLookupName: string;
-  newUser: FormGroup;
-  email: string;
-  readonly: boolean = true;
 
   data: any = {
     value: {
@@ -33,41 +26,30 @@ export class InventoryWarehousePage implements OnInit{
   };
   password: string;
   tenants: any = [];
-  productId: string;
-  product: any = {};
+  warehouseId: string;
+  warehouse: any = {};
 
   constructor(private route: ActivatedRoute, private router: Router, private _service: NotificationsService, public dialog: MdDialog) {}
 
   ngOnInit() {
-    console.log(this.readonly);
-    this.newUser = new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      email: new FormControl(''),
-      password: new FormControl('')
-    });
-
-
-
     this.route.params.subscribe((params: Params) => {
       console.log(params);
-      this.productId = params['id'];
+      this.warehouseId = params['id'];
       let query = {
-        _id: this.productId
+        _id: this.warehouseId
       };
 
-      MeteorObservable.call('findOne', 'products', query, {}).subscribe((res:any) => {
+      MeteorObservable.call('findOne', 'warehouses', query, {}).subscribe((res:any) => {
         console.log(res);
-        // this.product.name = res.name;
-        this.product = res;
-
+        this.warehouse = res;
+        console.log(this.warehouse.description)
       })
     });
   }
 
   onBlurMethod(field, value){
     let query = {
-      _id: this.productId
+      _id: this.warehouseId
     }
     let update = {
       $set: {
@@ -76,30 +58,30 @@ export class InventoryWarehousePage implements OnInit{
     };
     console.log(field);
     console.log(update);
-    MeteorObservable.call('update', 'products', query, update).subscribe(res => {
+    MeteorObservable.call('update', 'warehouses', query, update).subscribe(res => {
       console.log(res);
     })
   }
 
-  removeProduct() {
+  removeWarehouse() {
     let dialogRef = this.dialog.open(DialogSelect);
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         let query = {
-          _id: this.productId
+          _id: this.warehouseId
         };
         let update = {
           $set: {
             removed: true
           }
         };
-        MeteorObservable.call('update', 'products', query, update).subscribe(res => {
+        MeteorObservable.call('update', 'warehouses', query, update).subscribe(res => {
           console.log(res);
           this._service.success(
             'Success',
             'Removed Successfully'
           );
-          this.router.navigate(['/inventory/products']);
+          this.router.navigate(['/inventory/warehouses']);
           console.log('remove');
         });
 
@@ -122,86 +104,4 @@ export class InventoryWarehousePage implements OnInit{
     });
   }
 
-  returnResult(event) {
-
-    this.router.navigate(['/admin/users/' + event._id]);
-  }
-
-  addUser(user) {
-
-
-    let tenants = this.tenants.map(tenant => {
-      let temp = {
-        _id: tenant._id,
-        enabled: false,
-        groups: [""]
-      };
-      return temp;
-    });
-
-    if (user.valid) {
-      let newUser = {
-        username: user.value.email,
-        email: user.value.email,
-        profile: {
-          firstName: user.value.firstName,
-          lastName: user.value.lastName
-        },
-        password: user.value.password,
-        parentTenantId: Session.get('parentTenantId')
-      }
-
-      MeteorObservable.call('addUser', newUser).subscribe((res:any) => {
-        this._service[res.result](
-          res.subject,
-          res.message
-        )
-        if (res.result === 'success') {
-          this.router.navigate(['/admin/users', res.userId]);
-        }
-        // if (_id) {
-        //   let query = {
-        //     _id: _id
-        //   };
-        //   let update = {
-        //     $set:{
-        //       manages: [],
-        //       tenants: tenants,
-        //       parentTenantId: Session.get('parentTenantId')
-        //     }
-        //   };
-        //   let args = [query, update];
-        // }
-        // MeteorObservable.call('update', 'users', ...args).subscribe((res) => {
-        //   if (res) {
-        //     this._service.success(
-        //       'Success',
-        //       'Create a user successfully'
-        //     )
-        //     this.router.navigate(['/admin/users/' + _id]);
-        //   }
-        // });
-
-      });
-    }
-  }
-
-  removeReadonly() {
-    this.readonly = false;
-  }
-  //  //
-  // onChange(event) {
-  //   console.log(event);
-
-  // onChange(event) {
-  //   console.log(event);
-  //   let result = true;
-  //   if (event === true) {
-  //     result = false;
-  //   }
-  //   this.data = {
-  //     value : event,
-  //     hidden: result
-  //   }
-  // }
 }
